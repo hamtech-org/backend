5487489a03b0b9020acd78fbcb915a3473be5d70
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-04-28 22:17:29 +0700

    feat: add cloudfront configuration options to environment settings

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -12,6 +12,10 @@ export const s3Client = new S3Client({
 });
 
 export const S3_BUCKET = env.S3_BUCKET_NAME;
-export const CLOUDFRONT_URL = env.CLOUDFRONT_DOMAIN
-  ? `https://${env.CLOUDFRONT_DOMAIN}`
+export const CLOUDFRONT_URL = env.CLOUDFRONT_DOMAIN ? `https://${env.CLOUDFRONT_DOMAIN}` : '';
+export const CLOUDFRONT_PUBLIC_URL = env.CLOUDFRONT_PUBLIC_DOMAIN
+  ? `https://${env.CLOUDFRONT_PUBLIC_DOMAIN}`
+  : '';
+export const CLOUDFRONT_PRIVATE_URL = env.CLOUDFRONT_PRIVATE_DOMAIN
+  ? `https://${env.CLOUDFRONT_PRIVATE_DOMAIN}`
   : '';
